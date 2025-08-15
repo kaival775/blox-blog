eda5e93a440afdc4dcd509352ddfd30a68299bf9
kaivalya <kaivalya775@gmail.com>
2025-08-15 22:46:31 +0530

    Prepare for Vercel deployment

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:8000/api/v1" });
+const axiosInstance = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api/v1` : "http://localhost:8000/api/v1" 
+});
 
 axiosInstance.interceptors.request.use((req) => {
   const stringifyBlogData = window.localStorage.getItem("blog-data");
